dd42d5f20449de56b04e9ac098a8bd75a89c5849
noviceyash <noviceyash21@gmail.com>
2022-04-04 19:31:43 +0530

    Form,TextField Validation,Ink

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -10,124 +10,157 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   String name = "";
   bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(children: [
-          Image.asset(
-            "assets/images/login_page.png",
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Text(
-            "Welcome $name",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+        child: Form(
+          key: _formKey,
+          child: Column(children: [
+            SizedBox(
+              height: 40.0,
             ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter Username",
-                    labelText: "Username",
-                  ),
-                  onChanged: (value) {
-                    name = value;
-                    setState(() {});
-                  },
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter Password",
-                    labelText: "Password",
-                  ),
-                ),
-                SizedBox(
-                  height: 50.0,
-                ),
+            Image.asset(
+              "assets/images/sign_up_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome $name",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Username cannot be empty";
+                      }
 
-                InkWell(
-                  onTap: () async {
-                    setState(() {
-                      changeButton = true;
-                    });
-                    await Future.delayed(Duration(seconds: 1));
-                    Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  },
-                  //simple container
-                  // child: Container(
-                  //   width: 170,
-                  //   height: 50,
-                  //   alignment: Alignment.center,
-                  //   child: Text(
-                  //     "Sign Up",
-                  //     style: TextStyle(
-                  //       color: Colors.white,
-                  //       fontWeight: FontWeight.bold,
-                  //       fontSize: 18,
-                  //     ),
-                  //   ),
-                  //   decoration: BoxDecoration(
-                  //     color: Colors.deepOrange,
-                  //     borderRadius: BorderRadius.circular(8),
-                  //   ),
-                  // ),
+                      return null;
+                    },
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Password cannot be empty";
+                      } else if (value.length < 8) {
+                        return "Password should have at least 8 characters";
+                      }
 
-                  //animated container
-                  child: AnimatedContainer(
-                    duration: Duration(seconds: 1),
-                    width: changeButton ? 70 : 170,
-                    height: 50,
-                    alignment: Alignment.center,
-                    child: 
-                    changeButton
-                    ? Icon(
-                      Icons.done,
-                      color: Colors.white,
-                    ) 
-                    : 
-                    Text(
-                      "Sign Up",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                      return null;
+                    },
+                  ),
+
+                  SizedBox(
+                    height: 50.0,
+                  ),
+
+                  Material(
+                    color: Colors.deepOrange,
+                    borderRadius: BorderRadius.circular(changeButton ? 70 : 8),
+                    child: InkWell(
+                      onTap: () => moveToHome(context),
+                      //simple container
+                      // child: Container(
+                      //   width: 170,
+                      //   height: 50,
+                      //   alignment: Alignment.center,
+                      //   child: Text(
+                      //     "Sign Up",
+                      //     style: TextStyle(
+                      //       color: Colors.white,
+                      //       fontWeight: FontWeight.bold,
+                      //       fontSize: 18,
+                      //     ),
+                      //   ),
+                      //   decoration: BoxDecoration(
+                      //     color: Colors.deepOrange,
+                      //     borderRadius: BorderRadius.circular(8),
+                      //   ),
+                      // ),
+
+                      //animated container
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton ? 70 : 170,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: changeButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                        // decoration: BoxDecoration(
+                        //   color: Colors.deepOrange,
+                        //   shape: changeButton? BoxShape.circle : BoxShape.rectangle,
+                        //   // borderRadius: BorderRadius.circular(changeButton? 70 : 8),
+                        // ),
                       ),
                     ),
-                    decoration: BoxDecoration(
-                      color: Colors.deepOrange,
-                      shape: changeButton? BoxShape.circle : BoxShape.rectangle,
-                      // borderRadius: BorderRadius.circular(changeButton? 70 : 8),
-                    ),
                   ),
-                ),
 
-                // ElevatedButton(
-                //   child: Text("Sign Up"),
-                //   style: TextButton.styleFrom(minimumSize: Size(170, 50)),
-                //   onPressed: () {
-                //     Navigator.pushNamed(context, MyRoutes.homeRoute);
-                //   },
-                // ),
-              ],
-            ),
-          )
-        ]),
+                  // ElevatedButton(
+                  //   child: Text("Sign Up"),
+                  //   style: TextButton.styleFrom(minimumSize: Size(170, 50)),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  // ),
+                ],
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
