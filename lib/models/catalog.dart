411d5c4be63c57e8ea1a7e7293557601bc7d7df7
noviceyash <noviceyash21@gmail.com>
2022-04-13 23:54:50 +0530

    JSON Mapping useDataClassGenerator ProgressIndicator

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,15 +1,9 @@
+import 'dart:convert';
+
 class CatalogModel {
-  static final items = [
-  Item(
-    id: 1,
-    name: "Birthday card",
-    desc: "For Someone Special",
-    price: 80,
-    color: "#33505a",
-    image: "https://cdn.shopify.com/s/files/1/0583/3175/8777/products/8982B.jpg?v=1634120650"
-  )
-];
+  static List<Item>? items;
 }
+
 class Item {
   final int id;
   final String name;
@@ -18,6 +12,111 @@ class Item {
   final String color;
   final String image;
 
-  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
-}
+  Item(
+    this.id,
+    this.name,
+    this.desc,
+    this.price,
+    this.color,
+    this.image
+  );
 
+//Json Mapping using Data Class Generator plugin
+
+  Item copyWith({
+    int? id,
+    String? name,
+    String? desc,
+    num? price,
+    String? color,
+    String? image,
+  }) {
+    return Item(
+      id ?? this.id,
+      name ?? this.name,
+      desc ?? this.desc,
+      price ?? this.price,
+      color ?? this.color,
+      image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+  
+    result.addAll({'id': id});
+    result.addAll({'name': name});
+    result.addAll({'desc': desc});
+    result.addAll({'price': price});
+    result.addAll({'color': color});
+    result.addAll({'image': image});
+  
+    return result;
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      map['id']?.toInt() ?? 0,
+      map['name'] ?? '',
+      map['desc'] ?? '',
+      map['price'] ?? 0,
+      map['color'] ?? '',
+      map['image'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) => Item.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is Item &&
+      other.id == id &&
+      other.name == name &&
+      other.desc == desc &&
+      other.price == price &&
+      other.color == color &&
+      other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+      name.hashCode ^
+      desc.hashCode ^
+      price.hashCode ^
+      color.hashCode ^
+      image.hashCode;
+  }
+  }
+
+
+
+//Json Mapping manual way 
+// factory Item.fromMap(Map<String, dynamic> map) {
+//     return Item(
+//       id: map["id"],
+//       name: map["name"],
+//       desc: map["desc"],
+//       price: map["price"],
+//       color: map["color"],
+//       image: map["image"],
+//     );
+//   }
+
+//   toMap() => {
+//         "id": id,
+//         "name": name,
+//         "desc": desc,
+//         "price": price,
+//         "color": color,
+//         "image": image,
+//       };
