e42a50d1370d44314a9fcf80fca82504e7684183
noviceyash <noviceyash21@gmail.com>
2022-04-13 18:11:08 +0530

    Load & Decode JSON

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:scapes_bouquet/models/catalog.dart';
-
+import "dart:convert";
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Scapes Bouquet"),
@@ -14,9 +35,11 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index){
-            return ItemWidget(item: CatalogModel.items[index],);
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
           },
         ),
       ),
